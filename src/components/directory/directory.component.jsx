dccd443d6cb20ba8121bbb0e6c14b190a8e64933
Mael <mael.landrin@gmail.com>
2020-05-08 12:55:30 +0200

    Implemented basic routing with react-router-dom

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,8 @@ class Directory extends React.Component {
 
       <div className="directory-menu">
         {
-          this.state.sections.map(({title, imageUrl, size, id, linkUrl}) => {
-            return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
+          this.state.sections.map(({id, ...otherSectionProps}) => {
+            return <MenuItem key={id} {...otherSectionProps} />
           })
         }
       </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
 // Dynamic classes, style in JSX, JS methods in JSX
+// History and match = from props with React router. Accessed by using withRouter(nameOfComponent).
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div 
+      className={`${size} menu-item`} 
+      onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div 
       className="background-image"
       style={{
@@ -20,4 +24,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
